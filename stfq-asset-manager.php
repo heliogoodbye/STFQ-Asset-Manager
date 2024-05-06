f19bd93b31b865c951924eee2ef5ef47f381005f
Stelly <emailisfun@stel.ly>
2024-05-05 20:51:34 -0500

    Update stfq-asset-manager.php

--- stfq-asset-manager.php
+++ stfq-asset-manager.php
@@ -7,7 +7,7 @@ Version: 1.5
 Author: Strangefrequency LLC
 Author URI: https://strangefrequency.com/
 License: GPL-3.0-or-later
-License URI: https://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
 // Register custom post type
